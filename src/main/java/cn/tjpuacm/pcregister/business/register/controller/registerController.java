d668bd60e1467f700a9dffc4f9378c8c2d661ce8
ningxxxl <16847955@qq.com>
2018-10-21 21:09:04 +0800

    通过学号邮箱获取token

---
+++ src/main/java/cn/tjpuacm/pcregister/business/register/controller/registerController.java
@@ -0,0 +1,31 @@
+package cn.tjpuacm.pcregister.business.register.controller;
+
+import cn.tjpuacm.pcregister.util.JwtTokenUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ningxy
+ * @date 2018-10-21 19:46
+ */
+@RestController
+@RequestMapping("/register")
+public class registerController {
+
+    @Value("${jwt.properties.emailActivateExpiration}")
+    private long expiration;
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String register(@RequestParam("email") String email, @RequestParam("stu_id") String studentId) {
+        Map<String, Object> claimsMap = new HashMap<>();
+        claimsMap.put("email", email);
+        claimsMap.put("studentId", studentId);
+        return JwtTokenUtils.generateToken(claimsMap, expiration);
+    }
+}
