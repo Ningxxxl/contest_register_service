cea16912a414fcedb40e9824b0c7d99a02860223
ningxxxl <16847955@qq.com>
2018-11-14 15:21:48 +0800

    swagger

--- src/main/java/cn/tjpuacm/pcregister/business/account/controller/AccountController.java
+++ src/main/java/cn/tjpuacm/pcregister/business/account/controller/AccountController.java
@@ -3,6 +3,8 @@ package cn.tjpuacm.pcregister.business.account.controller;
 import cn.tjpuacm.pcregister.business.account.service.AccountService;
 import cn.tjpuacm.pcregister.entity.ResultBody;
 import cn.tjpuacm.pcregister.exception.GlobalErrorException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author ningxy
  * @date 2018-11-12 17:22
  */
+@Api(tags="账号API")
 @RestController
 @RequestMapping("/account")
 public class AccountController {
@@ -21,6 +24,7 @@ public class AccountController {
     @Autowired
     private AccountService accountService;
 
+    @ApiOperation(value="获取比赛账号", notes = "暂时是热身赛")
     @RequestMapping(method = RequestMethod.GET)
     public ResultBody getAccount(@Param("studentId") String studentId, @Param("phone") String phone) throws GlobalErrorException {
         if (StringUtils.isEmpty(studentId) || StringUtils.isEmpty(phone)) {
---
+++ src/main/java/cn/tjpuacm/pcregister/config/WebMvcConfigurerConfig.java
@@ -0,0 +1,45 @@
+package cn.tjpuacm.pcregister.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author ningxy
+ * @date 2018-11-14 14:37
+ */
+@Configuration
+@EnableWebMvc
+public class WebMvcConfigurerConfig implements WebMvcConfigurer {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+    }
+}
