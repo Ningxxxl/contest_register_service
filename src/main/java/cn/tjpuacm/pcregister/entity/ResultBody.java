e2516617773750c9c9630dc342fe804d09abba3d
ningxxxl <16847955@qq.com>
2018-11-15 16:00:19 +0800

    ResultBody泛型

--- src/main/java/cn/tjpuacm/pcregister/entity/ResultBody.java
+++ src/main/java/cn/tjpuacm/pcregister/entity/ResultBody.java
@@ -2,7 +2,8 @@ package cn.tjpuacm.pcregister.entity;
 
 import cn.tjpuacm.pcregister.enums.GlobalErrorEnum;
 import cn.tjpuacm.pcregister.exception.GlobalErrorInterface;
-import lombok.AllArgsConstructor;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -11,23 +12,35 @@ import lombok.Data;
  * @author ningxy
  * @date 2018-10-24 17:23
  */
+@ApiModel(value = "返回体")
 @Data
-@AllArgsConstructor
-public class ResultBody {
+public class ResultBody<T> {
     /**
      * 响应代码
      */
+    @ApiModelProperty(value = "状态码", notes = "0表示成功，非0表示异常", example = "0")
     private Integer code;
 
     /**
      * 响应消息
      */
+    @ApiModelProperty(value = "返回信息", example = "success")
     private String message;
 
     /**
      * 响应结果
      */
-    private Object data;
+    @ApiModelProperty(value = "数据")
+    private T data;
+
+    public ResultBody() {
+    }
+
+    public ResultBody(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
 
     public ResultBody(Integer code, String message) {
         this.code = code;
@@ -39,11 +52,25 @@ public class ResultBody {
         this.message = errorInfo.getMessage();
     }
 
-    public static ResultBody generateSuccessResult(Object dataObj) {
-        return new ResultBody(
-                GlobalErrorEnum.SUCCESS.getCode(),
-                GlobalErrorEnum.SUCCESS.getMessage(),
-                dataObj
-        );
+    public ResultBody<T> setCode(Integer code) {
+        this.code = code;
+        return this;
+    }
+
+    public ResultBody<T> setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public ResultBody<T> setData(T data) {
+        this.data = data;
+        return this;
+    }
+
+    public static <T> ResultBody<T> generateSuccessResult(T dataObj) {
+        return new ResultBody<T>()
+                .setCode(GlobalErrorEnum.SUCCESS.getCode())
+                .setMessage(GlobalErrorEnum.SUCCESS.getMessage())
+                .setData(dataObj);
     }
 }
