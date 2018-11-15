5120f2bd036388068f68d978e2a749a56911a6b9
ningxxxl <16847955@qq.com>
2018-11-15 16:09:06 +0800

    完善API注释

--- src/main/java/cn/tjpuacm/pcregister/system/user/controller/SysUserController.java
+++ src/main/java/cn/tjpuacm/pcregister/system/user/controller/SysUserController.java
@@ -2,6 +2,8 @@ package cn.tjpuacm.pcregister.system.user.controller;
 
 import cn.tjpuacm.pcregister.system.user.service.SysUserService;
 import cn.tjpuacm.pcregister.system.user.vo.SysUserVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author ningxy
  * @date 2018-10-21 10:26
  */
+@Api(tags = "用户管理")
 @RestController
 @RequestMapping(value = "/user")
 public class SysUserController {
@@ -25,6 +28,7 @@ public class SysUserController {
      * @throws InstantiationException
      * @throws IllegalAccessException
      */
+    @ApiOperation(value = "获取用户信息")
     @RequestMapping(value = "", method = RequestMethod.GET)
     public SysUserVO getUser(SysUserVO userVO) throws InstantiationException, IllegalAccessException {
         return sysUserService.getUser(userVO);
--- src/main/java/cn/tjpuacm/pcregister/system/user/vo/SysUserVO.java
+++ src/main/java/cn/tjpuacm/pcregister/system/user/vo/SysUserVO.java
@@ -1,5 +1,7 @@
 package cn.tjpuacm.pcregister.system.user.vo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,17 +10,27 @@ import lombok.NoArgsConstructor;
  * @author ningxy
  * @date 2018-10-21 10:35
  */
+@ApiModel
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SysUserVO {
+    @ApiModelProperty(value = "businessId")
     private Long id;
+    @ApiModelProperty(value = "邮箱")
     private String email;
+    @ApiModelProperty(value = "学号")
     private String studentId;
+    @ApiModelProperty(value = "学校")
     private String school;
+    @ApiModelProperty(value = "学院")
     private String institute;
+    @ApiModelProperty(value = "专业")
     private String major;
+    @ApiModelProperty(value = "班级")
     private String clazz;
+    @ApiModelProperty(value = "真实姓名")
     private String realName;
+    @ApiModelProperty(value = "手机号码")
     private String phone;
 }
